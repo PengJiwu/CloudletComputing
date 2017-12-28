04364b3cf594e376b3853ee275b2ad8ddf6c30c6
calmar3 <m_calzetta@live.com>
2017-12-28 13:50:09 +0100

    test

--- src/main/java/core/StartSimulation.java
+++ src/main/java/core/StartSimulation.java
@@ -2,6 +2,6 @@ package core;
 
 public class StartSimulation {
     public static void main(String[] args) {
-        System.out.printf("Hello World");
+        System.out.printf("Hello World!");
     }
 }
